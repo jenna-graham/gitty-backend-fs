b9931cf0599b5c11bf96312bc4e8a0e47751e4db
jenna-graham <jclayto2@gmail.com>
2022-08-12 11:04:47 -0700

    GET route to display posts TEST passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,8 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+const agent = request.agent(app);
+
 jest.mock('../lib/services/github');
 
 describe('auth routes', () => {
@@ -23,6 +25,21 @@ describe('auth routes', () => {
       iat: expect.any(Number),
       exp: expect.any(Number),
     });
+  });
+
+  it('GET /api/v1/posts lists all posts for authenticated user', async () => {
+    await agent.get('/api/v1/github/callback?code=42');
+    const res = await agent.get('/api/v1/posts');
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual(
+      expect.arrayContaining([
+        {
+          id: expect.any(String),
+          title: expect.any(String),
+          post: expect.any(String),
+        },
+      ])
+    );
   });
 
   afterAll(() => {
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,6 +1,7 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
 DROP TABLE IF EXISTS github_user;
+DROP TABLE IF EXISTS new_post;
 
 CREATE TABLE github_user (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -8,3 +9,12 @@ CREATE TABLE github_user (
     email TEXT,
     avatar TEXT
 );
+
+CREATE TABLE new_post (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    title TEXT NOT NULL,
+    post TEXT NOT NULL
+);
+
+INSERT INTO new_post (title, post) VALUES 
+('Hello', 'I am new to SQL PLEASE HELP!')
